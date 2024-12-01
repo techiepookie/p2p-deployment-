5774d423cebf8509eb020be9f051636bb9c70ed5
techiepookie <24227514010314@paruluniversity.ac.in>
2024-12-01 13:03:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO, join_room, emit
-from urllib.parse import quote  # Use this instead of deprecated `url_quote`
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3f82bc47a95f40c3b94e5a7f25d83cf649a2f8bdc22b6e34'
@@ -8,7 +8,7 @@ socketio = SocketIO(app)
 
 @app.route('/<room_id>')
 def room(room_id):
-    return render_template('chat.html', room_id=quote(room_id))
+    return render_template('chat.html', room_id=room_id)
 
 @socketio.on('joinRoom')
 def handle_join_room(data):
@@ -28,5 +28,6 @@ def handle_message(data):
         emit('receive_message', {'message': message, 'avatar': avatar}, room=room, include_self=False)
 
 if __name__ == '__main__':
+    # Use the PORT environment variable or fallback to 4000 for local testing
     port = int(os.environ.get('PORT', 4000))
     socketio.run(app, host='0.0.0.0', port=port, debug=True)
